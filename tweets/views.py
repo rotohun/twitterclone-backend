89440a90db153f682be10403b6cd3ab7d35d5057
Ronald Hunter <contact@ronaldhunter.co>
2020-06-13 23:15:24 -0400

    added tweet django app

---
+++ tweets/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render
+
+from .models import Tweet
+# Create your views here.
+
+def home_view(request, *args, **kwargs):
+   # return HttpResponse("<h1> Hello World </h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+def detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by Javascript
+    return json data
+    """
+
+    data = {
+      "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status) # json.dumps content_type
+
